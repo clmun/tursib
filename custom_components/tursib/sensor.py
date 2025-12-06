61d4caec2ad0ede466931e144b89baf6036e49fb
clmun <clmun01@gmail.com>
2025-12-06 13:39:30 +0200

    schimbat modul de calcul minute pana la plecare

--- custom_components/tursib/sensor.py
+++ custom_components/tursib/sensor.py
@@ -1,5 +1,5 @@
 import datetime
-import math
+# import math
 import logging
 import aiohttp
 from bs4 import BeautifulSoup
@@ -101,10 +101,12 @@ class TursibCoordinator(DataUpdateCoordinator):
                 return None, None
 
         delta = (dep_dt - now_dt).total_seconds()
-        if delta < 60:
+        if delta < 30:
             minutes = "Acum"
         else:
-            minutes = str(math.ceil(delta / 60))
+            # minutes = str(math.ceil(delta / 60))
+            # minutes = str(round(delta / 60))  # rotunjire la cel mai apropiat minut
+            minutes = str(int(delta // 60))
         return minutes, dep_dt
 
     def _sorted_departures(self, departures, now_dt):
